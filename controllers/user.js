f77dd1b3c21fc176d40e1e0002c9431a64673d81
Johan Viancha <johan.viancha@clickonplans.com>
2022-02-25 08:56:16 -0500

    Se creo el controlador de user

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,30 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const userGet = (req, res) =>{
+const userGet = (req = request, res) =>{
+    const params = req.query;
+
     res.json({
-        msg:'get APIff -controlador'
+        msg:'get APIff -controlador',
+        params
     });
 }
 
 const userPost =(req, res) =>{
+
+    const {nombre, edad} = req.body;
+
     res.json({
-        msg:'post APIff - controlador'
+        msg:'post APIff - controlador',
+        nombre,
+        edad
     });
 }
 
 const userPut = (req, res) =>{
+
+    const id = req.params.id;
     res.json({
-        msg:'put APIff - controlador'
+        msg:'put APIff - controlador' + id
     });
 }
 
