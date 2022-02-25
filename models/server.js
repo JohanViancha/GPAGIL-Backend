7f16b32509703c2550869a04924e162d66f983e1
Johan Viancha <johan.viancha@clickonplans.com>
2022-02-25 11:52:31 -0500

    Ajuste en la conexión de base de datos

--- models/server.js
+++ models/server.js
@@ -23,9 +23,9 @@ class Server{
     }
 
     conectarDB(){
-        pool.connect().then(res=>{
+        /*pool.connect().then(res=>{
             console.log('Conexión de base de datos establecida')
-        })
+        })*/
     }
 
     middleeares(){
@@ -45,7 +45,7 @@ class Server{
 
 
     listen(){
-        this.app.listen(this.port,()=>{
+        this.app.listen(this.port || 3000,()=>{
             console.log("El servidor está corriendo ", this.port);
         })
     }
